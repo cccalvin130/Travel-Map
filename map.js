c0b8886577512c73e26a035ecec57b8e6157cf6d
Calvin Loo Hui Le <CALVIN.LOO.HUI@student.mmu.edu.my>
2026-08-27 22:30:00 +0800

    Main-Map.

---
+++ map.js
@@ -0,0 +1,5 @@
+var map = L.map('map').setView([2.9278072, 101.6419120], 10);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; OpenStreetMap contributors'
+}).addTo(map);
