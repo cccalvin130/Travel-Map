4726d7d4c6345afdad96ce14b08c9efa2f85c33b
Calvin Loo Hui Le <CALVIN.LOO.HUI@student.mmu.edu.my>
2026-08-30 21:32:29 +0800

    Add Marker Functions.

--- map.js
+++ map.js
@@ -3,3 +3,7 @@ var map = L.map('map').setView([2.9278072, 101.6419120], 10);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; OpenStreetMap contributors'
 }).addTo(map);
+
+var marker = L.marker([2.9278072, 101.6419120]).addTo(map);
+
+marker.bindPopup("MMU Cyberjaya");
