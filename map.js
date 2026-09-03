5a6e6636a24b7a84128d31e6c52e53108ab23d13
Calvin Loo Hui Le <CALVIN.LOO.HUI@student.mmu.edu.my>
2026-09-03 22:36:13 +0800

    add country, note ,visited date,image function;use if function to image for visited place;use ' function to do show list in html;use for each function for image and += for image function; use $ function  to put variable in other variable.

--- map.js
+++ map.js
@@ -7,33 +7,57 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 var places = [
     {
         name: "Tokyo",
+        country:"Japan",
         latitude: 35.6762,
-        longitude: 139.6503
+        longitude: 139.6503,
+        date:"20/12/2025",
+        note:"My First trip to Japan!"
     },
     {
         name: "Kuala Lumpur",
+        country:"Malaysia",
         latitude: 3.1390,
-        longitude: 101.6869
-    },
-    {
-        name: "Singapore",
-        latitude: 1.3521,
-        longitude: 103.8198
-    },
-    {
-        name: "Bangkok",
-        latitude: 13.7563,
-        longitude: 100.5018
+        longitude: 101.6869,
+        date:"1/2/2026",
+        note:"First time to KL"
     },
     {
         name:"MMU Cyberjaya",
+        country:"Malaysia",
         latitude:2.9278072,
-        longitude:101.641912
+        longitude:101.641912,
+        date:"3/9/2026",
+        note:"My Foundation University!",
+        images:[
+            "image/business .jpg",
+            "image/business 2.jpg",
+            "image/business 3.jpg"
+        ]
+
     }
 ];
     
 places.forEach(function(place) {
+
+    var photos = "";
+
+    if (place.images) {
+
+        place.images.forEach(function(image) {
+            photos += `<img src="${image}" width="150">`;
+        });
+}
+
+    var popupContent = `
+        <h3>${place.name}, ${place.country}</h3>
+
+        ${photos}
+
+        <p>Visited: ${place.date}</p>
+        <p>${place.note}</p>
+    `;
+
     L.marker([place.latitude, place.longitude])
         .addTo(map)
-        .bindPopup(place.name);
+        .bindPopup(popupContent);
 });
