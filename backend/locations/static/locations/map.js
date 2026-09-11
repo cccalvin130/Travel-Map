d6d47ce837b981406a55398a40d89fb11a64c3be
Calvin Loo Hui Le <CALVIN.LOO.HUI@student.mmu.edu.my>
2026-09-12 00:08:24 +0800

    Travel History done half

--- backend/locations/static/locations/map.js
+++ backend/locations/static/locations/map.js
@@ -11,7 +11,34 @@ fetch('/api/locations/')
     .then(data => {
 
         var places = data.locations;
+        var history = document.getElementById('travel-history');
         places.forEach(function(place){
+
+            history.innerHTML += `
+                <div id="history-${place.id}"> 
+                    <h3>${place.name}</h3>
+                    <p>${place.city}, ${place.country}</p>
+                    <p>Visited: ${place.visit_date}</p>
+                    <p>${place.notes}</p>
+                </div>
+            `;
+        });    
+
+        places.forEach(function(place) {
+
+            document.getElementById(`history-${place.id}`).addEventListener('click', function() {
+
+                map.setView([place.latitude, place.longitude], 15);
+
+                document.getElementById('map').scrollIntoView({
+                    behavior: 'smooth'
+                });
+
+            });
+
+        });
+
+         places.forEach(function(place) {
 
             var photos="";
 
