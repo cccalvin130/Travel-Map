c0a3159eea2d617693c283c192df9bfd3c62bd09
Calvin Loo Hui Le <CALVIN.LOO.HUI@student.mmu.edu.my>
2026-09-25 02:44:13 +0800

    Fixed marker bug

--- backend/locations/static/locations/map.js
+++ backend/locations/static/locations/map.js
@@ -1,4 +1,15 @@
+var defaultMarkerIcon = L.icon({
+    iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
+    iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
+    shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
 
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+});
+
+L.Marker.prototype.options.icon = defaultMarkerIcon;
 
 var map = L.map('map').setView([2.9278072, 101.6419120], 5);
 
